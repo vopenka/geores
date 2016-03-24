670f581f9062e713b9262e144f3a36b511b816f3
vopenka <vopenka@gmail.com>
2016-03-24 09:37:40 +0000

    sql edits

---
+++ src/main/resources/sql/postcodes.sql
@@ -0,0 +1,7 @@
+﻿-- Transform to wgs84
+
+SELECT gid,postcode,ST_Transform(geom,4326) AS geom
+	INTO postcodes_4326
+	FROM postcodes_27700;
+
+ALTER TABLE postcodes_4326 ADD PRIMARY KEY (gid);
--- src/main/resources/sql/streets.sql
+++ src/main/resources/sql/streets.sql
@@ -1,5 +1,15 @@
-﻿
--- First create views for temporary storage of data
+﻿-- First create views for temporary storage of data
+
+CREATE MATERIALIZED VIEW streets_view AS
+SELECT row_number() OVER () AS id, foo.name AS name, ST_Transform(ST_ClosestPoint(geom, ST_Centroid(foo.geom)),4326) AS geom
+FROM (SELECT a.name, ST_LineMerge(ST_Collect(a.way)) AS geom, ST_Touches(a.way,b.way) AS touch
+	FROM planet_osm_line AS a LEFT JOIN planet_osm_line AS b ON ST_Touches(a.way,b.way) AND a.name = b.name 
+	WHERE ST_Touches(a.way,b.way)
+	GROUP BY ST_Touches(a.way,b.way), a.name
+	UNION
+	SELECT a.name, a.way AS geom, ST_Touches(a.way,b.way) AS touch
+	FROM planet_osm_line AS a LEFT JOIN planet_osm_line AS b ON ST_Touches(a.way,b.way) AND a.name = b.name 
+	WHERE ST_Touches(a.way,b.way) IS NOT true) AS foo;
 
 CREATE MATERIALIZED VIEW countries AS 
 	SELECT row_number() OVER () AS id, name, ST_Transform(way, 4326) AS geom
@@ -30,32 +40,33 @@ CREATE MATERIALIZED VIEW towns AS
 -- use first ceremonials as counties.
 
 SELECT s.id AS id, s.name AS street, ctr.name AS country, cer.name AS county, s.geom AS geom 
-	INTO streets2
-	FROM streets AS s 
+	INTO streets
+	FROM streets_view AS s 
 	LEFT JOIN countries AS ctr ON ST_Contains(ctr.geom, s.geom) 
 	LEFT JOIN ceremonials AS cer ON ST_Contains(cer.geom, s.geom)
+	LEFT JOIN postcodes_4326 AS pc ON ST_Contains(pc.geom, s.geom)
 	WHERE s.name IS NOT NULL
 LIMIT 5000;
 
-ALTER TABLE streets2 ADD PRIMARY KEY (id);
+ALTER TABLE streets ADD PRIMARY KEY (id);
 
 -- Overide ceremonials by real counties. Ceremonials stay only in case there is no county in place
 
-UPDATE streets2 AS s
+UPDATE streets AS s
 	SET county=ct.name
 	FROM counties AS ct
-	WHERE ST_Contains(ct.geom, s.geom)
+	WHERE ST_Contains(ct.geom, s.geom);
 
 -- Add cities
 
-ALTER TABLE streets2 ADD COLUMN city text;
+ALTER TABLE streets ADD COLUMN city text;
 
 -- Spatial join closests city,town,village to the street
 
-UPDATE streets2 AS s
+UPDATE streets AS s
 	SET city=jtown.name
 	FROM (SELECT DISTINCT ON(s.id) s.id AS id, t.name AS name
-		FROM streets2 AS s, towns AS t
+		FROM streets AS s, towns AS t
 		WHERE ST_DWithin(s.geom, t.geom, 50000)
 		ORDER BY s.id, ST_Distance(s.geom, t.geom)) jtown
 		WHERE s.id = jtown.id;
